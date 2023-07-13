71d8e02c4c4553e5fe16a762db2ed3b5676c5148
Tomiwa Kukoyi <tomiwakukoyiweb@gmail.com>
2023-07-13 17:20:57 +0100

    Last touches

---
+++ src/screens/Plans/Plans.jsx
@@ -0,0 +1,7 @@
+import "./Plans.css";
+import { useState } from "react";
+const Plans = () => {
+  return <div className="plansScreen"></div>;
+};
+
+export default Plans;
--- src/screens/Profile/Profile.jsx
+++ src/screens/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import Nav from "../../components/nav/Nav";
 import "./Profile.css";
 import { auth } from "../../firebase";
 import { selectUser } from "../../features/userSlice";
+import Plans from "../Plans/Plans";
 
 const Profile = () => {
   const user = useSelector(selectUser);
@@ -16,16 +17,18 @@ const Profile = () => {
             src="https://th.bing.com/th?q=Netflix+Avatar+Logo&w=120&h=120&c=1&rs=1&qlt=90&cb=1&pid=InlineBlock&mkt=en-WW&cc=NG&setlang=en&adlt=moderate&t=1&mw=247n"
             alt=""
           />
-        </div>
-        <div className="profileScreen__details">
-          <h2>{user.email}</h2>
-          <div className="profileScreen__plans">
-            <button
-              onClick={() => auth.signOut()}
-              className="profileScreen__signOut"
-            >
-              Sign Out
-            </button>
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <h3>Plans</h3>
+              <Plans />
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
+            </div>
           </div>
         </div>
       </div>
